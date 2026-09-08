067033d3f69761b7d20bade7a21d99f164e82f54
YASWANTH S M <yaswanthsm.al24@bitsathy.ac.in>
2026-09-08 09:51:30 +0530

    Time: 1361 ms (5.82%), Space: 15.6 MB (16.5%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.py
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -4,4 +4,8 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        count=0
+        for i in range(1,n+1):
+            count+=(len(str(i))-1)//3
+        return count
         
